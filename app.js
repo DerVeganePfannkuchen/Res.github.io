08f90b76cb576c2fed9d83bee748a92ec2c1de97
DerVeganePfannkuchen <shirode@icloud.com>
2023-12-08 18:38:01 -0500

    add new features
    add the about and contact pop up and a button to close each. the when
    one window is open and you hit the other the open one will close and the
    other will open

--- app.js
+++ app.js
@@ -1,8 +1,50 @@
 const jake = document.getElementsByClassName("exSelect")
 const boxA = document.getElementsByClassName("box")
-console.log(boxA)
 var dateClick = 0
+const FooterIcon = document.getElementsByClassName("imgIcon")
+const Windows = document.getElementsByClassName("HiddenIconWindow")
+for(let x=0; x < FooterIcon.length;x++){
+    console.log(x, FooterIcon[x])
+}
 
+for(let x =0; x< FooterIcon.length; x++){
+    FooterIcon[x].addEventListener('click', event =>{
+        if(event.target.classList.contains('About') && !(event.target.classList.contains('open')) && FooterIcon[2].classList.contains('open')){
+            Windows[1].classList.add('windowClose')
+            Windows[1].classList.remove('windowOpen')
+            FooterIcon[2].classList.remove('open')
+            setTimeout(RemoveWindow,500, 2) 
+            setTimeout(AddWindow, 500, x)
+               
+        } else if(event.target.classList.contains('About') && !(event.target.classList.contains('open'))){
+            AddWindow(x)
+               
+        } else if(event.target.classList.contains('About') && event.target.classList.contains('open')){
+            Windows[x-1].classList.add('windowClose')
+            Windows[x-1].classList.remove('windowOpen')
+            FooterIcon[x].classList.remove('open')
+            setTimeout(RemoveWindow,500, x) 
+
+        } else if(event.target.classList.contains('contact') && !(event.target.classList.contains('open')) && FooterIcon[1].classList.contains('open')){
+            Windows[0].classList.add('windowClose')
+            Windows[0].classList.remove('windowOpen')
+            FooterIcon[1].classList.remove('open')
+            setTimeout(RemoveWindow,500, 1) 
+            setTimeout(AddWindow, 500, x)
+               
+        } else if (event.target.classList.contains('contact') && !(event.target.classList.contains('open'))){
+            AddWindow(x)   
+
+        }else if(event.target.classList.contains('contact') && event.target.classList.contains('open')){
+            Windows[x-1].classList.add('windowClose')
+            Windows[x-1].classList.remove('windowOpen')
+            FooterIcon[x].classList.remove('open')
+            setTimeout(RemoveWindow,500, x) 
+        } else {
+            return
+        }
+    })
+}
 
 
 for(let x=0; x < jake.length;x++){
@@ -70,7 +112,24 @@ if (boxA[0].classList.contains('active') ){
 
 
 }
-
+}
+function RemoveWindow (x){
+    Windows[x-1].classList.remove('windowClose')
 }
 
-
+function AddWindow(x){
+    Windows[x-1].classList.add('windowOpen')
+    FooterIcon[x].classList.add('open')
+}
+function RemoveAbout(){
+            Windows[0].classList.add('windowClose')
+            Windows[0].classList.remove('windowOpen')
+            FooterIcon[1].classList.remove('open')
+            setTimeout(RemoveWindow,500, 1) 
+}
+function RemoveContact(){
+    Windows[1].classList.add('windowClose')
+    Windows[1].classList.remove('windowOpen')
+    FooterIcon[2].classList.remove('open')
+    setTimeout(RemoveWindow,500, 2) 
+}
