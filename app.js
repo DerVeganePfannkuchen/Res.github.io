51b7b6b60466783b191d54859f5c174b7c201347
DerVeganePfannkuchen <shirode@icloud.com>
2023-12-29 20:18:33 -0500

    add more transtions and all the windows

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const icons = document.getElementsByClassName("icon")
+const content = document.getElementsByClassName("content")
 const main =document.querySelector('.mainIcons')
 const welcome =document.querySelector('.welcomeBox')
 const welcomeClose =document.querySelector('.closeWelcome')
@@ -6,15 +7,67 @@ const footer =document.querySelector('.footerNav')
 const X =document.querySelector('.X')
 const footerIcons =document.getElementsByClassName("footerIcon")
 const iconLables =document.getElementsByClassName("iconLables")
-setTimeout(welcomeOut,10000)
+const closeContentX = document.getElementsByClassName("closeContent")
+const title = document.getElementsByClassName("title")
+const footerWindows =document.getElementsByClassName("footerWindows")
+const closeFooter =document.getElementsByClassName("closeFooterWindow")
+const XF =document.getElementsByClassName("XF")
+const WindowTimeout = setTimeout(welcomeOut,10000)
+console.log(closeFooter[0].classList + "hey bitch")
+
+console.log(WindowTimeout)
 console.log(main.querySelectorAll('.icon'))
 main.addEventListener('click', event =>{
     var images = [].slice.call(main.querySelectorAll('.icon'),0); // get all images inside frame1, and convert to array
     let index =images.indexOf(event.target)
+    content[index].classList.add("show")
+    content[index].classList.add("fadeInIcons")
+    closeContentX[index].classList.add("fadeInIcons")
+    iconsOut()
+})
+
+footer.addEventListener('click', event =>{
+    var images = [].slice.call(footer.querySelectorAll('.footerIcon'),0); // get all images inside frame1, and convert to array
+    var images2 = [].slice.call(footer.querySelectorAll('.title'),0); // get all images inside frame1, and convert to array
+    let index =images.indexOf(event.target)
+    let index2 =images2.indexOf(event.target)
+    console.log("first " +index)
+    console.log("second " +index2)
+    if(index == 1 && !(footerWindows[1].classList.contains('hidden')) ){
+        footerWindows[0].classList.remove('hidden')
+        closeFooterWindow(1)
+    } else if(index2 == 1 && !(footerWindows[1].classList.contains('hidden')) ){
+        footerWindows[0].classList.remove('hidden')
+        closeFooterWindow(1)
+    }else if(index == 2 && !(footerWindows[0].classList.contains('hidden'))){
+        footerWindows[1].classList.remove('hidden')
+        closeFooterWindow(0)
+    } else if(index2 == 2 && !(footerWindows[0].classList.contains('hidden'))){
+        footerWindows[1].classList.remove('hidden')
+        closeFooterWindow(0)
+    } else if(index == 2 && !(footerWindows[1].classList.contains('hidden'))){
+        closeFooterWindow(1)
+    }else if(index2 == 2 && !(footerWindows[1].classList.contains('hidden'))){
+        closeFooterWindow(1)
+    } else if(index == 1 && !(footerWindows[0].classList.contains('hidden'))){
+        closeFooterWindow(0)
+    }else if(index2 == 1 && !(footerWindows[0].classList.contains('hidden'))){
+        closeFooterWindow(0)
+    }else if(index == 1){
+        footerWindows[0].classList.remove('hidden')
+    }else if(index2 == 1){
+        footerWindows[0].classList.remove('hidden')
+    }else if(index == 2){
+        footerWindows[1].classList.remove('hidden')
+        console.log('qwee')
+    }else if(index2 == 2){
+        footerWindows[1].classList.remove('hidden')
+        console.log('qwee')
+    }
 })
 
 function welcomeOut(){
-    if(!welcome.classList.contains('hidden')){
+    clearTimeout(WindowTimeout)
         setTimeout(hiddWelcome =>{
         welcome.classList.add('hidden');
     },900)
@@ -22,15 +75,14 @@ function welcomeOut(){
     welcomeClose.classList.add('welcomeDown');
     X.classList.add('welcomeDown');
     home();
-}else{
-    return;
-}
+
+
     
 }
 function home(){
     main.classList.remove('hidden');
     footer.classList.remove('hidden');
-    footer.classList.add('fadeInIcons');
+    footer.classList.add('fadeFooterIcons');
    for(let x=0; x< icons.length;x++){
         icons[x].classList.add('fadeInIcons')
     };
@@ -40,7 +92,7 @@ function home(){
     for(let x=0; x< iconLables.length;x++){
         iconLables[x].classList.add('fadeInWords')
    };
-   setTimeout(done =>{
+   let delay =setTimeout(done =>{
     footer.classList.remove('fadeInIcons');
     for(let x=0; x< icons.length;x++){
          icons[x].classList.remove('fadeInIcons')
@@ -52,5 +104,72 @@ function home(){
          iconLables[x].classList.remove('fadeInWords')
     };
     console.log("im out")
-   },1500)
+   },799)
+   
+}
+function iconsOut(){
+
+    
+   for(let x=0; x< icons.length;x++){
+        icons[x].classList.add('fadeOutIcons')
+    };
+    for(let x=0; x< iconLables.length;x++){
+        iconLables[x].classList.add('fadeOutWords')
+   };
+   
+    
+}
+
+function closeContent(index){
+    iconsOut()
+    if(!(main.classList.contains('hidden'))){
+        main.classList.add('hidden');
+    }
+    setTimeout(x =>{
+        content[index].classList.remove("show")
+        content[index].classList.remove("fadeOutIcons")
+        closeContentX[index].classList.remove("fadeOutIcons")
+    },799)
+    content[index].classList.remove("fadeInIcons")
+    closeContentX[index].classList.remove("fadeInIcons")
+    iconsHome()
+    content[index].classList.add("fadeOutIcons")
+    closeContentX[index].classList.add("fadeOutIcons")
+   
+}
+function iconsHome(){
+    
+    main.classList.remove('hidden');
+   for(let x=0; x< icons.length;x++){
+        icons[x].classList.remove('fadeOutIcons')
+        icons[x].classList.add('fadeInIcons')
+    };
+    for(let x=0; x< iconLables.length;x++){
+        iconLables[x].classList.remove('fadeOutWords') 
+        iconLables[x].classList.add('fadeInWords')
+   };
+   setTimeout(done =>{
+    footer.classList.remove('fadeInIcons');
+    for(let x=0; x< icons.length;x++){
+         icons[x].classList.remove('fadeInIcons')
+     };
+     for(let x=0; x< iconLables.length;x++){
+         iconLables[x].classList.remove('fadeInWords')
+    };
+    console.log("im out")
+   },799)
+}
+function closeFooterWindow(index){
+
+    footerWindows[index].classList.add('welcomeDown')
+    closeFooter[index].classList.add('welcomeDown')
+    XF[index].classList.add('welcomeDown')
+    setTimeout(x =>{
+        footerWindows[index].classList.add('hidden')
+        footerWindows[index].classList.remove('welcomeDown')
+        XF[index].classList.remove('welcomeDown')
+        closeFooter[index].classList.remove('welcomeDown')
+
+    }, 400)
+    
 }
